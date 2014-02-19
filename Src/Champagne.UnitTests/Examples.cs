aadc0bc85642608c15ae5ef54c0cf67ac83af5b8
Mark Seemann <mark@ploeh.dk>
2014-02-19 21:08:01 +0100

    Added a single example (test),
    and an incredibly wrong implementation. While the library will be written
    in C# (for purposes of easier reuse), most tests will actually be written
    in F# using FsCheck (or, at least, that's the plan right now).
    
    The C# unit tests are mostly for example purposes.

---
+++ Src/Champagne.UnitTests/Examples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Grean.Champagne.UnitTests
+{
+    public class Examples
+    {
+        [Fact]
+        public void ReplaceNumberInSequence()
+        {
+            var seq = new[] { 4, 2, 42, 1337, 7 };
+            var actual = seq.Replace(9, i => i == 2);
+            Assert.Equal(new[] { 4, 9, 42, 1337, 7 }, actual);
+        }
+    }
+}
---
+++ Src/Champagne/Sequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grean.Champagne
+{
+    public static class Sequence
+    {
+        public static IEnumerable<T> Replace<T>(
+            this IEnumerable<T> source,
+            T item,
+            Func<T, bool> equalityComparer)
+        {
+            return new[] { 4, 9, 42, 1337, 7 }.Cast<T>();
+        }
+    }
+}

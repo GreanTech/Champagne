6fd5b4d2ef88da1b4a2ce1a5d2aef46a9a7bd68b
Mark Seemann <mark@ploeh.dk>
2014-02-20 14:31:56 +0100

    Added AssemblyInfo to F# unit tests.

---
+++ Src/Champagne.FSharp.UnitTests/Properties/AssemblyInfo.fs
@@ -0,0 +1,45 @@
+﻿module Grean.Champagne.FSharp.UnitTests
+
+open System
+open System.Reflection
+open System.Resources
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Unit tests for Champagne for F#")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Grean ApS")>]
+[<assembly: AssemblyProduct("Champagne")>]
+[<assembly: AssemblyCopyright("Copyright © Grean ApS 2014")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("5254DD18-A252-4128-AD47-86C8CB510118")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+[<assembly: NeutralResourcesLanguage("en")>]
+
+()
+
+
